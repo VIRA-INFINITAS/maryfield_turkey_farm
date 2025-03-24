465c9a4034838eae5e4008445ec39dabf6c00175
Radoslav Vidis <159483107+VIRA-INFINITAS@users.noreply.github.com>
2025-03-24 12:01:27 +0000

    Add navigation and hero section

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// NAVIGATION SCROLL
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
