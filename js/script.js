9df54c7415f1ef74e1f78d8ff2fe3eabb93720fb
ViRaDeX <159483107+ViRaDeX@users.noreply.github.com>
2025-04-13 20:45:19 +0100

    Change nav bar scrolling behaviour

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-// CHANGE NAVBAR BG COLOUR ON HOME PAGE WHEN SCROLL DOWN
+// CHANGE NAVBAR BG COLOUR ON HOME PAGE WHEN SCROLL DOWN + HIDE ON SCROLL DOWN (DESKTOP ONLY)
+
 const navbar = document.querySelector(".navbar");
 
+let lastScrollTop = 0;
+
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 56) {
-    navbar.classList.add("scrolled");
-  } else {
+  const currentScroll = window.scrollY;
+
+  if (currentScroll <= 10) {
+    // When at top – transparent navbar, visible
     navbar.classList.remove("scrolled");
+    navbar.classList.remove("navbar-hidden");
+  } else if (currentScroll > lastScrollTop) {
+    // Scrolling down
+    navbar.classList.add("navbar-hidden");
+  } else {
+    // Scrolling up
+    navbar.classList.remove("navbar-hidden");
+    navbar.classList.add("scrolled");
   }
+
+  lastScrollTop = currentScroll;
 });
